30051624002cb42a43969c7049413e51bf0673af
nachito <elnachoescachondo@gmail.com>
2021-11-08 23:59:55 -0300

    roles

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,14 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth'
 import { Router } from "@angular/router";
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/compat/firestore';
+
+export interface userInterface{
+  id?: string,
+  email?: string,
+  admin?: boolean
+}
+
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +16,7 @@ import { Router } from "@angular/router";
 export class AuthService {
 
   constructor(private AFauth : AngularFireAuth,
-    private router: Router) { }
+    private router: Router, private afs: AngularFirestore) { }
 
   login(email: string, password: string){
 
@@ -28,9 +36,25 @@ export class AuthService {
 
   register(email: string, password: string){
     return new Promise ((resolve, reject)=>{
-    this.AFauth.createUserWithEmailAndPassword(email, password).then(res =>{
-      resolve(res)
-    }).catch( err=> reject(err))
-  })
+    this.AFauth.createUserWithEmailAndPassword(email, password)
+    .then(res =>{ 
+      resolve(res),
+      this.updateUserData(res.user)
+    }).catch( err=> console.log(reject(err)))
+  });
+  }
+  
+  private updateUserData(user){
+    const userRef: AngularFirestoreDocument<any> = this.afs.doc(`users/${user.uid}`);
+    const data: userInterface = {
+      id: user.uid,
+      email: user.email,
+      admin: false
+    }
+    return userRef.set(data, {merge:true})
+  }
+
+  isAdmin(userUid){
+    return this.afs.doc<userInterface>(`users/${userUid}`).valueChanges();
   }
 }
